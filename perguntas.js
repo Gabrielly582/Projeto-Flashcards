a42d90ca7065f38437b929cf8a4c6f62f8597550
Gabrielly582 <00001116740291sp@Al.educacao.sp.gov.br>
2024-11-26 16:49:26 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação.'
+)
+
+criaCartao(
+    'Geografia',
+    'Quantos continentes existem?',
+    'Existem 6 continentes: América, Europa, África, Ásia, Oceania e Antártida.'
+)
+
+criaCartao(
+    'Inglês',
+    'O que a palavra legend significa em português?',
+    'A palavra legend significa lenda.'
+)
+
+criaCartao(
+    'Português',
+    'O que é um verbo?',
+    'Verbo é uma classe gramatical que exprime principalmente ações, estados e fenômenos.'
+)
+
+criaCartao(
+    'História',
+    'Em que ano o Brasil se tornou uma república?',
+    'O Brasil se tornou uma república no ano de 1889.'
+)
+
+criaCartao(
+    'Matemática',
+    'O que é uma incógnita?',
+    'Uma incógnita é um valor desconhecido que deve ser determinado para resolver uma equação ou inequação.'
+)
